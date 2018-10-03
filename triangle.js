0b337cb6dc03c32535af35d128c00b4373aafcaf
Yixing Lao <yixing.lao@gmail.com>
2018-10-03 11:17:40 -0700

    regenerate triangle.js

--- triangle.js
+++ triangle.js
@@ -1,6 +1,6 @@
 var TriangleExample;
 (function (TriangleExample) {
-    var MouseController = (function () {
+    var MouseController = /** @class */ (function () {
         function MouseController(canvas, area) {
             this.canvas = canvas;
             this.area = area;
@@ -16,19 +16,16 @@ var TriangleExample;
         MouseController.prototype.getLocalCoord = function (ev) {
             return { x: ev.clientX - this.canvas.offsetLeft, y: ev.clientY - this.canvas.offsetTop };
         };
-
         MouseController.prototype.onMouseDown = function (ev) {
             var e = this.getLocalCoord(ev);
             this.isMouseDown = true;
             this.lastMouseCoord = e;
         };
-
         MouseController.prototype.onMouseUp = function (ev) {
             var e = this.getLocalCoord(ev);
             this.isMouseDown = false;
             this.lastMouseCoord = null;
         };
-
         MouseController.prototype.onMouseMove = function (ev) {
             var e = this.getLocalCoord(ev);
             if (this.isMouseDown) {
@@ -37,11 +34,9 @@ var TriangleExample;
                 this.area.pan(offset);
             }
         };
-
         MouseController.prototype.onMouseLeave = function (ev) {
             this.isMouseDown = false;
         };
-
         MouseController.prototype.onMouseWheel = function (ev) {
             ev.wheelDelta = ev.deltaY * 120.0;
             var e = this.getLocalCoord(ev);
@@ -49,25 +44,22 @@ var TriangleExample;
             return false;
         };
         return MouseController;
-    })();
+    }());
     TriangleExample.MouseController = MouseController;
 })(TriangleExample || (TriangleExample = {}));
 ///<reference path="input.ts"/>
 var TriangleExample;
 (function (TriangleExample) {
-    
-
     //bindings to C++ functions
-    var Bindings = (function () {
+    var Bindings = /** @class */ (function () {
         function Bindings() {
         }
         Bindings.initGL = Module.cwrap('initGL', 'number', ['number', 'number']);
         Bindings.drawTriangle = Module.cwrap('drawTriangle', '', ['number']);
         return Bindings;
-    })();
-
+    }());
     //a helper for some JS-to-Emscripten conversions
-    var HeapUtils = (function () {
+    var HeapUtils = /** @class */ (function () {
         function HeapUtils() {
         }
         HeapUtils.floatArrayToHeap = function (arr) {
@@ -77,10 +69,9 @@ var TriangleExample;
             return arrayPointer;
         };
         return HeapUtils;
-    })();
-
+    }());
     //our program that draws a triangle
-    var Program = (function () {
+    var Program = /** @class */ (function () {
         function Program(canvas) {
             this.canvas = canvas;
             //current translation of the triangle
@@ -91,10 +82,8 @@ var TriangleExample;
                 console.log("Could not initialise GL");
                 return;
             }
-
             //get the mouse listen to canvas mouse events
             this.mouseController = new TriangleExample.MouseController(canvas, this);
-
             //request redraw
             this.invalidate();
         }
@@ -108,29 +97,24 @@ var TriangleExample;
             this.translation.originY -= glOffset.y;
             this.invalidate();
         };
-
         //zoom by the given ratio
         Program.prototype.zoom = function (ratio, origin) {
             this.translation.zoom *= ratio;
             this.invalidate();
         };
-
         //render the scene
         Program.prototype.render = function () {
             //convert the JS translation object to an emscripten array of floats
             var translationPtr = HeapUtils.floatArrayToHeap([this.translation.originX, this.translation.originY, this.translation.zoom]);
-
             //call the native draw function
             Bindings.drawTriangle(translationPtr);
-
             //free the array memory
             _free(translationPtr);
         };
-
         Program.prototype.invalidate = function () {
             window.requestAnimationFrame(this.render.bind(this));
         };
         return Program;
-    })();
+    }());
     TriangleExample.Program = Program;
 })(TriangleExample || (TriangleExample = {}));
